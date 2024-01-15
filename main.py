1dcb7cb484f07a56e7cb51591e8feeb32834e54f
EvandroSanches <evandrosanches62@gmail.com>
2024-01-15 17:15:50 -0300

    Tratando Outliers e utilizando ensemble

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from keras.models import Sequential, load_model
-from keras.layers import Dense, Dropout
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
-from scikeras.wrappers import KerasRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from sklearn.compose import make_column_transformer
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingRegressor
+from sklearn.metrics import accuracy_score
+from pyod.models.knn import KNN
 from pandasgui import show
+import pickle
 
 epochs = 40
 batch_size = 100
-
-def CriaRede():
-    #Criando modelo
-    modelo = Sequential()
-
-    modelo.add(Dense(units=150, activation='relu', input_dim=22))
-    modelo.add(Dropout(0.2))
-    modelo.add(Dense(units=150, activation='relu'))
-    modelo.add(Dropout(0.2))
-    modelo.add(Dense(units=150, activation='relu'))
-    modelo.add(Dropout(0.2))
-    modelo.add(Dense(units=1, activation='linear'))
-
-    modelo.compile(optimizer='adam', loss='mae', metrics=['mse'])
-    return modelo
 
 def Carregar_Dados():
     #Preparando base de dados
@@ -40,15 +25,20 @@ def Carregar_Dados():
     dados = dados[dados.price > 800]
     dados = dados[dados.price < 350000]
 
-    #Separando dados previsores e target
-    previsores = dados.drop('price', axis=1)
-    target = dados.iloc[:, 0].values
-
     #Codificando previsores com metodo OneHotEncoder (Previsores categoricos nominais)
     encoder = make_column_transformer(
                 (OneHotEncoder(handle_unknown='ignore'), ['vehicleType','gearbox', 'fuelType', 'notRepairedDamage']),
                 remainder='passthrough', sparse_threshold=False)
-    previsores = encoder.fit_transform(previsores)
+    dados = encoder.fit_transform(dados)
+
+    #Removendo dados outliers
+    dados = pd.DataFrame(dados)
+    outliers = Outliers(dados)
+    dados = dados.drop(index=outliers)
+
+    #Separando dados previsores e target
+    previsores = dados.drop(dados.columns[19], axis=1)
+    target = dados.iloc[:, 19].values
 
     #Normalizando dados
     normalizador = MinMaxScaler()
@@ -57,23 +47,41 @@ def Carregar_Dados():
     return previsores, target
 
 
+def Features(previsores, target):
+    #Retorna um array com o valor de importancia de cada feature
+    modelo = RandomForestClassifier()
+    modelo.fit(previsores, target)
+    resultado = modelo.feature_importances_
+
+    return resultado
+def Outliers(dados):
+    #Encontra dados outliers através do algoritmo KNN
+    detector = KNN()
+    detector.fit(dados)
+
+    labels = detector.labels_
+
+    outliers = []
+
+    #Pegando indice dos valores classificados como outlier
+    for i in range(len(labels)):
+        if labels[i] == 1:
+            outliers.append(i)
+
+    return outliers
+
 def Treinamento():
     previsores, target = Carregar_Dados()
 
-    #Definindo callbacks
-    es = EarlyStopping(monitor='loss', patience=10, verbose=1, min_delta= 1e-10)
-    rlp = ReduceLROnPlateau(monitor='loss', patience=5, verbose=1)
-    md_encoder = ModelCheckpoint(monitor='loss', filepath='Modelo.0.1', save_best_only=True, verbose=1)
-
-    #Validação Cruzada
-    modelo = KerasRegressor(build_fn=CriaRede,
-                            epochs=epochs,
-                            batch_size=batch_size,
-                            callbacks=[es, rlp, md_encoder])
-
+    modelo = HistGradientBoostingRegressor(max_iter=epochs)
 
     resultados = cross_val_score(estimator=modelo,
                                  X=previsores, y=target, cv=5, scoring='neg_mean_absolute_error')
+
+    modelo.fit(previsores, target)
+
+    with open('Modelo.0.1.pkl', 'wb') as file:
+        pickle.dump(modelo, file)
 
     #Apresentando dados de treino
     media = resultados.mean()
@@ -85,19 +93,22 @@ def Treinamento():
     plt.ylabel('Perda')
     plt.show()
 
-
 def Predict():
     previsores, target = Carregar_Dados()
 
-    modelo = load_model('Modelo.0.1')
+    with open('Modelo.0.1.pkl', 'rb') as f:
+        modelo = pickle.load(f)
 
     resultado = modelo.predict(previsores)
 
     target = np.expand_dims(target, axis=1)
+    resultado = np.expand_dims(resultado, axis=1)
 
     resultado = np.concatenate((resultado, target), axis=1)
 
     predict = pd.DataFrame(data=resultado, columns=['Previsão', 'Preço'])
+    predict['Diferença'] = predict.Preço - predict.Previsão
+
     show(predict)
 
 Predict()
